6a57952a05557e226e215205af9c5ad05f045506
Murilo <rodrigues.murilo2006@gmail.com>
2025-11-30 18:42:20 -0300

    Preparando codigo para o deploy

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,7 @@ import pacientesRouter from './routes/pacientes.routes';
 import cors from 'cors';
 
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT || 3000;
 // Middleware para habilitar o CORS
 app.use(cors());
 
